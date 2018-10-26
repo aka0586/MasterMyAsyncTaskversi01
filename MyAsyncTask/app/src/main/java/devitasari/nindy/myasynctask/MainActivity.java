0d7522def8da3a92db10913999d489c898843a75
“aka0586” <akhmad.0586@students.amikom.ac.id>
2018-10-26 10:27:15 +0700

    Versi 01.1.0
    java dan xml

---
+++ MyAsyncTask/app/src/main/java/devitasari/nindy/myasynctask/MainActivity.java
@@ -0,0 +1,13 @@
+package devitasari.nindy.myasynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
